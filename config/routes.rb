c88ab68ea68f28139ad36a2f2c9d110540d18956
Anthony Chiu <anthony9086@hotmail.com>
2021-12-12 15:36:37 +0800

    finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#index'
+  # root to: '../views/pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :users, only: %i[index show]
-      resources :items, only: %i[index show destroy create new] do
-        resources :requests, only: %i[index show destroy create new]
-
-
+      resources :items, only: %i[index show new create destroy] do
+        resources :requests, only: %i[index show new create destroy]
       end
-        reou
       post 'login', to: 'users#login'
     end
   end
-
 end
