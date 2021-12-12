9e342942357aa270e130d4e33c3d12090eb824e1
Anthony Chiu <anthony9086@hotmail.com>
2021-12-12 22:33:54 +0800

    corrected users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :requests, dependent: :destroy
+  acts_as_token_authenticatable
   validates :nickname, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  acts_as_token_authenticatable
 end
