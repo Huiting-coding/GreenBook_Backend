bf210d9481f16df8526ec74e3cd755e33a4a5af7
Huiting Qiu <277080574@qq.com>
2021-12-13 21:13:50 +0800

    generate new request model and controller

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,4 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :item
-  validates :user_id, :item_id, presence: true
 end
--- db/migrate/20211213041231_create_requests.rb
+++ db/migrate/20211213041231_create_requests.rb
@@ -1,8 +1,8 @@
 class CreateRequests < ActiveRecord::Migration[6.0]
   def change
     create_table :requests do |t|
-      t.reference :user
-      t.reference :item
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
 
       t.timestamps
     end
