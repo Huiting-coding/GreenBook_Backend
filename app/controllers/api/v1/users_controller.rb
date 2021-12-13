1f99ae7462b83c6d198acfd3bc79ac0d44e70df0
Huiting Qiu <277080574@qq.com>
2021-12-13 10:09:39 +0800

    controller modified

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,12 +20,8 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   end
 
-  # def index
-  #   @users = User.all
-  # end
 
-<<<<<<< HEAD
-=======
+
   def show
     set_user
     @user
@@ -34,24 +30,10 @@ class Api::V1::UsersController < Api::V1::BaseController
   def new
   @user = User.new
   end
->>>>>>> df5a871b303db0562a0acab39519d93448987413
-
-  # def show
-  #   set_user
-  #   @user
-  # end
 
 
-<<<<<<< HEAD
 
-  # private
-
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
-=======
   def set_user
     @user = User.find(params[:id])
   end
->>>>>>> df5a871b303db0562a0acab39519d93448987413
 end
