6bceca2f85977c5980689815e31285b12fec5961
Huiting Qiu <277080574@qq.com>
2021-12-14 00:17:11 +0800

    remove category-id as foreign key to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_041231) do
+ActiveRecord::Schema.define(version: 2021_12_13_161516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(version: 2021_12_13_041231) do
     t.string "condition"
     t.bigint "user_id", null: false
     t.boolean "available"
-    t.bigint "category_id", null: false
-    t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -57,7 +55,6 @@ ActiveRecord::Schema.define(version: 2021_12_13_041231) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
   add_foreign_key "requests", "items"
   add_foreign_key "requests", "users"
